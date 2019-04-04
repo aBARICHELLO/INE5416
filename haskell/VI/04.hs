a5648d4873b703753c52ad80b20e0745fdfa4070
Barichello <artur@barichello.me>
2019-04-04 14:47:27 -0300

    complete 4

--- haskell/VI/04.hs
+++ haskell/VI/04.hs
@@ -1,7 +1,3 @@
---
--- FINISH
---
-
 data Arvore = Null | No Int Arvore Arvore
 
 minhaArvore :: Arvore
@@ -30,22 +26,56 @@ minimoElemento (No n esq dir) =
     minimo n (minimo (minimoElemento esq) (minimoElemento dir))
 
 -- /// A ///
-ocorrenciasElemento ::  Arvore -> Int -> Int
+ocorrenciasElemento :: Arvore -> Int -> Int
 ocorrenciasElemento Null _ = 0
 ocorrenciasElemento (No n esq dir) x
-    | (n == Nil) = x
-    | otherwise = (ocorrenciasElemento minhaArvore esq) || (ocorrenciasElemento minhaArvore dir)
-ocorrenciasElemento (No n esq dir) x
-    | (n == x) = x + ocorrenciasElemento minhaArvore 0
-    | otherwise = ocorrenciasElemento minhaArvore 0
+    | (n == x) = 1 + (ocorrenciasElemento esq x) + (ocorrenciasElemento dir x)
+    | otherwise = (ocorrenciasElemento esq x) + (ocorrenciasElemento dir x)
 
 -- /// B ///
--- maioresQueElemento ::  Arvore -> Int -> Int
--- maioresQueElemento ::
+maioresQueElemento :: Arvore -> Int -> Int
+maioresQueElemento Null _ = 0
+maioresQueElemento (No n esq dir) x
+    | (n > x) = 1 + (maioresQueElemento esq x) + (maioresQueElemento dir x)
+    | otherwise = (maioresQueElemento esq x) + (maioresQueElemento dir x)
+
+-- /// C ///
+mediaElementos :: Arvore -> Float
+mediaElementos Null = 0.0
+mediaElementos arv = fromIntegral (somaElementos arv) / fromIntegral (quantidade arv)
+
+-- /// D ///
+quantidade :: Arvore -> Int
+quantidade Null = 0
+quantidade (No n esq dir) = 1 + (quantidade esq) + (quantidade dir)
+
+-- /// E ///
+elementos ::  Arvore -> [Int]
+elementos Null = []
+elementos (No n esq dir) = [n] ++ elementos esq ++ elementos dir
 
 main = do
     print (somaElementos minhaArvore)
     print (buscaElemento minhaArvore 30)
     print (buscaElemento minhaArvore 55)
     print (minimoElemento minhaArvore)
-    print (ocorrenciasElemento minhaArvore 0)
+
+    print "Exercicio A"
+    print (ocorrenciasElemento Null 0)
+    print (ocorrenciasElemento minhaArvore 32)
+
+    print "Exercicio B"
+    print (maioresQueElemento Null 0)
+    print (maioresQueElemento minhaArvore 0)
+
+    print "Exercicio C"
+    print (mediaElementos Null)
+    print (mediaElementos minhaArvore)
+
+    print "Exercicio D"
+    print (quantidade Null)
+    print (quantidade minhaArvore)
+
+    print "Exercicio E"
+    print (elementos Null)
+    print (elementos minhaArvore)
