14a111b214d6fc7f6b62a35e8018343e42572c05
Barichello <artur@barichello.me>
2019-04-13 17:44:39 -0300

    Enums

--- haskell/VII/08.hs
+++ haskell/VII/08.hs
@@ -1,7 +1,20 @@
-data Color = Red | Green | Blue deriving Enum
+-- Implementing Enum with data
+-- and deriving other functionalities
+data Color = Red | Green | Blue | Purple
+    deriving (Eq, Ord, Show, Read, Enum)
 
 color1 :: Color
 color1 = Red
 
+main :: IO()
 main = do
+    -- Deriving from Show
     print color1
+    -- Deriving from Read
+    print (read "Purple"::Color)
+    -- Deriving from Ord
+    print (Green < Blue)
+    -- Deriving from Enum
+    print (pred Green)
+    print (succ Red)
+    print [Red .. Purple]
