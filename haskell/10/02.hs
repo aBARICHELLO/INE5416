b3b8ba5dfa6161b016670debfb7295ef743c16eb
BARICHELLO <artur@barichello.me>
2019-04-26 01:24:24 -0300

    Finish 01 start 02

--- haskell/10/02.hs
+++ haskell/10/02.hs
@@ -1,10 +1,5 @@
-data Expr = Val Int | Div Expr Expr | Add Expr Expr | Sub Expr Expr | Mult Expr Expr
-
--- As outras implementações seriam muito piores de dar manutenção, sendo esta a mais fácil.
--- No modo sem utilizar monads teríamos um 'switch case hell' onde o código possuiria vários níveis
--- de indentação, diminuindo a legibilidade.
--- O modo de implementação com monads ainda sofre do mesmo problema só que com funções lambda e vários níveis
--- do operador >>=
+-- f
+data Expr = Val Int | Div Expr Expr | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Sqrt Expr
 
 eval :: Expr -> Maybe Int
 eval (Val n) = return n
@@ -16,7 +11,7 @@ eval (Sub x y) = do
     n <- eval x
     m <- eval y
     mysub n m
-eval (Mult x y) = do
+eval (Mul x y) = do
     n <- eval x
     m <- eval y
     mymult n m
@@ -24,6 +19,9 @@ eval (Div x y) = do
     n <- eval x
     m <- eval y
     mydiv n m
+eval (Sqrt x) = do
+    x <- eval x
+    mysqrt x
 
 myadd :: Int -> Int -> Maybe Int
 myadd n m = Just (n + m)
@@ -38,9 +36,13 @@ mydiv :: Int -> Int -> Maybe Int
 mydiv n m | m == 0 = Nothing
           | otherwise = Just (n `div` m)
 
+mysqrt :: Int -> Maybe Int
+mysqrt x | x < 0 = Nothing
+         | otherwise = Just ( sqrt x)
+
 main :: IO()
 main = do
     print (eval ( Add (Val 28) (Val 2) ))
     print (eval ( Add (Sub (Val 2) (Val 2)) (Val 2) ))
-    print (eval ( Mult (Div (Add (Val 28) (Val 2)) (Sub (Val 6) (Val 1))) (Val 3) ))
+    print (eval ( Mul (Div (Add (Val 28) (Val 2)) (Sub (Val 6) (Val 1))) (Val 3) ))
     print (eval ( Div (Val 2) (Val 2) ))
