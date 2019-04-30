255dc791ac1f65a23ea0ab8b8a04508fe8b30b2e
Barichello <artur@barichello.me>
2019-04-30 14:39:26 -0300

    Fix matrix get

--- haskell/09/08.hs
+++ haskell/09/08.hs
@@ -4,7 +4,10 @@ main = do
     -- Matrices module tests
     print emptyMatrix
     print identityMatrix
-    print ((get identityMatrix 0 0) == 0 && (get identityMatrix 3 3) == 8)
+    print ((get identityMatrix 0 0) == 1)
+    print ((get identityMatrix 0 1) == 0)
+
+    print "Exercises:"
 
     -- // 8A //
     print (elementSum emptyMatrix)
--- haskell/09/Matrices.hs
+++ haskell/09/Matrices.hs
@@ -18,13 +18,12 @@ emptyMatrix = Mat [] 0 0
 
 -- 2x2 identity matrix
 identityMatrix :: Matrix Int
-identityMatrix = Mat [1, 1, 1, 1] 2 2
+identityMatrix = Mat [1, 0, 0, 1] 2 2
 
 -- Get an element from coordinate x y
 -- NOTE: Coordinates start at (1,1)
 get :: Matrix Int -> Int -> Int -> Int
-get (Mat a _ _) x y | ((x == 0) || (y == 0)) = 0
-                    | otherwise = (a !! x * y - 1)
+get (Mat a row col) x y = a !! (x * row + y) -- (i*n+j)
 
 -- Returns the sum of all elements in the matrix array
 elementSum :: Matrix Int -> Int
@@ -37,9 +36,17 @@ sumList (x:xs) = x + sumList xs
 
 -- Sum two matrices
 sumMatrix :: Matrix Int -> Matrix Int -> Matrix Int
-sumMatrix (Mat a0 x0 y0) (Mat a1 x1 y1) | (x0 /= x1) || (y0 /= y1) = emptyMatrix
-                                  | otherwise = Mat (auxSum a0 a1) x0 y0
+sumMatrix (Mat a0 x0 y0) (Mat a1 x1 y1)
+    | (x0 /= x1) || (y0 /= y1) = emptyMatrix
+    | otherwise = Mat (auxSum a0 a1) x0 y0
 
+-- Aux function of sumMatrix
 auxSum :: [Int] -> [Int] -> [Int]
 auxSum [] [] = []
 auxSum (x:xs) (y:ys) = [x + y] ++ auxSum xs ys
+
+-- isMagicSquare :: Matrix Int -> Bool
+-- isMagicSquare (Mat a x y) =
+
+-- hasRepeatedElements :: Matrix Int -> Bool
+
