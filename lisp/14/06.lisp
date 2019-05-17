5ec6c43c6645d7f11008513715b9a7c7592eb493
Barichello <artur@barichello.me>
2019-05-17 01:09:41 -0300

    Finish list 14 ex 06

--- lisp/14/06.lisp
+++ lisp/14/06.lisp
@@ -1,5 +1,3 @@
-;; (\x y z -> if x > y && x > z then x else if y > x && y > z then y else z)
-
 (defun _map(fn l)
     (if (null l)
         ()
@@ -8,21 +6,7 @@
 )
 
 (defun main ()
-    (setq x (read))
-    (setq y (read))
-    (setq z (read))
-
-    (write-line (write-to-string (
-        (lambda (x y z)
-            (cond
-                ((and (> x y) (> x z)) x)
-                ((and (> y x) (> y z)) y)
-                (t z)
-            )
-        ) x y z
-    )))
+    (write-line (write-to-string (_map (function (lambda (x) (= 0 (mod x 2)))) '(0 1 2 3 4 5))))
 )
 
 (main)
-
-;; f
